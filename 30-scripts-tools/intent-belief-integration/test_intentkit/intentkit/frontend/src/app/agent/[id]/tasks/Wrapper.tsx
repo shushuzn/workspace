f661263054fa41ffb8c7d602f0db293466505e24
shushuzn <132275809+shushuzn@users.noreply.github.com>
2026-03-18 00:28:16 +0800

    清理：删除 116 个空目录 + 52 个测试文件

--- 30-scripts-tools/intent-belief-integration/test_intentkit/intentkit/frontend/src/app/agent/[id]/tasks/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const ClientPage = dynamic(() => import("./ClientPage"), { ssr: false });
-
-export default function AgentTasksWrapper() {
-  return <ClientPage />;
-}
