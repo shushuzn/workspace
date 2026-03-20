cf544dda75fbc0c045643c867aa49a1d58858677
shushuzn <132275809+shushuzn@users.noreply.github.com>
2026-03-20 18:28:30 +0800

    notifications" - 2026-03-20 18:28

--- temp_check_tools.py
+++
@@ -1,6 +0,0 @@
-import json
-d = json.load(open('30-scripts-tools/tools_registry.json', encoding='utf-8'))
-tools = d.get('tools', {})
-sa_tools = [k for k in tools.keys() if k.startswith('SA')]
-print('Total tools:', len(tools))
-print('SA tools:', sa_tools)
